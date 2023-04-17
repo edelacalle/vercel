8cb7485cad5eb0755a76d5b51558c3ea50380e6a
edelacalle <enrique@delacalle.net>
2023-04-17 18:33:00 +0200

    s0

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+'use client'
+import Link from 'next/link';
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+//import {Button, ButtonGroup, Card, Dropdown, ProgressBar} from 'react-bootstrap';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>Hello World! This is the Home pag2 <Button>HOLA</Button></p>
+      <p>
+        Visit the <Link href="/about">About</Link> page.
+      </p>
+    </div>
+  )
+}
+
+export default Home
