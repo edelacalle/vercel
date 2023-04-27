0bc320f2306a40587e69aee2c80522eddb2fae24
edelacalle <81438155+edelacalle@users.noreply.github.com>
2023-04-27 23:00:52 +0200

    Actualizar _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,6 @@
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  console.log("EN APP");
+  console.log("EN APP2");
   return <Component {...pageProps} />
 }
