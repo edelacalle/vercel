a5ba93a72b55d657507ca93e24f481c73a05ea89
edelacalle <enrique@delacalle.net>
2023-04-27 23:21:06 +0200

    b0
    s

--- next-i18next.config.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = {
-  // debug: true,
-  i18n: {
-    defaultLocale: 'es',
-    locales: ['en','es'],
-  },
-}
--- src/pages/_app.js
+++
@@ -1,6 +0,0 @@
-import '@/styles/globals.css'
-
-export default function App({ Component, pageProps }) {
-  console.log("EN APP2");
-  return <Component {...pageProps} />
-}
