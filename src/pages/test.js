03d90a979acdaec11497e5614ca2cc574779d257
edelacalle <enrique@delacalle.net>
2023-04-28 18:51:03 +0200

    pub3

--- src/pages/test.js
+++ src/pages/test.js
@@ -2,7 +2,12 @@ import { Layout } from '@/components/Layout/Layout';
 import Link from "next/link";
 
 function myBody(props){
-  return <Link href={"https://api.vercel.com/v1/integrations/deploy/prj_NaxXMohIPlZSyz9PgqP1qrLMeSDI/4AU8JxuYTC"}> Refrescar </Link>
+  return <>
+    <Link href={"https://api.vercel.com/v1/integrations/deploy/prj_NaxXMohIPlZSyz9PgqP1qrLMeSDI/4AU8JxuYTC"}> Refrescar </Link>
+    <Link href={"https://coxenergy.sharepoint.com/:b:/s/pubdoc/Ed_ZZvOotrdEjly7okKkL58BiVds73X4aDmmSdlwv4NqwQ?e=4%3a1aQFpF&at=9"}> Doc PDF </Link>
+
+  
+  </>
   
 }
 
